dfdc901aae6597093e9796a4236b3516df60b5ae
ameshalexk <ameshalexusa@gmail.com>
2021-06-21 14:27:03 -0500

    wip

--- models/Place.js
+++ models/Place.js
@@ -1,42 +1,42 @@
 const mongoose = require('mongoose');
-const geoCoder = require('../utils/geocoder');
+// const geoCoder = require('../utils/geocoder');
 
 const PlaceSchema = new mongoose.Schema({
-    address: {
-        type: String,
-        required: [true, 'Please add an address']
+  address: {
+    type: String,
+    required: [true, 'Please add an address'],
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'],
     },
-    location: {
-        type: {
-            type: String,
-            enum: ['Point']
-        },
-        coordinates: {
-            type: [Number],
-            index: '2dsphere'
-        },
-        formattedAddress: String,
-        city: String
+    coordinates: {
+      type: [Number],
+      index: '2dsphere',
     },
-    createdAt: {
-        type: Date,
-        default: Date.now()
-    }
+    formattedAddress: String,
+    city: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 // Before saving, convert address to geoCode
-PlaceSchema.pre('save', async function(next) {
-    const loc = await geoCoder.geocode(this.address);
-    this.location = {
-        type: 'Point',
-        coordinates: [loc[0].longitude, loc[0].latitude],
-        city: loc[0].city,
-        formattedAddress: loc[0].formattedAddress
-    };
+// PlaceSchema.pre('save', async function(next) {
+//     const loc = await geoCoder.geocode(this.address);
+//     this.location = {
+//         type: 'Point',
+//         coordinates: [loc[0].longitude, loc[0].latitude],
+//         city: loc[0].city,
+//         formattedAddress: loc[0].formattedAddress
+//     };
 
-    // Do not save address
-    this.address = undefined;
-    next();
-});
+//     // Do not save address
+//     this.address = undefined;
+//     next();
+// });
 
 module.exports = mongoose.model('Place', PlaceSchema);
