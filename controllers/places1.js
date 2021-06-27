82461a5d392a6d832b211c3cacf98c05f745e89b
ameshalexk <ameshalexusa@gmail.com>
2021-06-26 23:26:49 -0500

    weather api for apple shortccut done check

--- controllers/places1.js
+++ controllers/places1.js
@@ -54,7 +54,7 @@ exports.getPlaces1 = async (req, res, next) => {
       let hourly = result.data;
 
       const final = hourly.filter((hour) => {
-        if (hour.precip > 0.4) {
+        if (hour.precip > 0.9) {
           return true;
         }
       });
